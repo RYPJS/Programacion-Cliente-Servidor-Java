927443037311c46b596242edf95d89485690ab0c
Crowned08 <87036156+Crowned08@users.noreply.github.com>
2024-04-02 22:37:21 -0600

    Creacion de la estructura

---
+++ Fide_Airlines/src/main/java/com/mycompany/fide_airlines/Fide_Airlines.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fide_airlines;
+
+/**
+ *
+ * @author danie
+ */
+public class Fide_Airlines {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
