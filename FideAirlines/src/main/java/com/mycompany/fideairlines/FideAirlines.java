2f4693e793f075423674276e1c3afdc2fdd0bac3
kendall <kendallgomezmolina16@gmail.com>
2024-04-03 21:05:45 -0600

    New project

---
+++ FideAirlines/src/main/java/com/mycompany/fideairlines/FideAirlines.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fideairlines;
+
+/**
+ *
+ * @author kenda
+ */
+public class FideAirlines {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
