9ebc5fc39d7460610fbaef4123f67a2a7d159ee4
RYPJS <159687913+RYPJS@users.noreply.github.com>
2024-04-16 13:02:03 -0600

    Delete FideAirlines directory
    ejemplo merege

--- FideAirlines/src/main/java/com/mycompany/fideairlines/Client/Runable/ClientApp.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.fideairlines.Client.Runable;
-import com.mycompany.fideairlines.Client.View.Menu;
-public class ClientApp {
-    public static void main(String[] args) {
-        Menu m = new Menu();
-        m.setVisible(true);
-        
-    }
-}
